989ca78370aa44b3b831c30d95c0c5d08d9d8139
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-10-28 16:51:33 +0300

    refactor: change tag selectors to class and id selectors (3.4 Tag selectors)

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
 
 
     //and appending.
+    listItem.className="task";
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
     listItem.appendChild(editInput);
